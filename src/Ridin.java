ca30591a1a0533f865b39c2bdde0f36db1ab4dd4
HP <mdzaid1299@gmail.com>
2022-09-04 02:40:06 +0530

    end commit

--- src/Ridin.java
+++ src/Ridin.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Ridin {
+public class Ridin {
+    //this autoboxing
+    public static void main(String[] args) {
+        int num = 7;
+        Integer ObjNum = num;
+        System.out.println(ObjNum);
+
+        //wrapper class obj to primiitive
+        Integer aNum = 4;
+        int num2 = aNum.intValue();
+        System.out.println(num2);
+
+        // converting int primitive type into wrapper object
+        Integer bNum = Integer.valueOf(num2);
+        System.out.println(bNum);
+    }
+
 }
